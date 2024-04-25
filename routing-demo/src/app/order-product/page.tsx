a67e1516a7f88f712452b2e38640e8b8cb2ef0fd
jcob-sikorski <jm.siekiera@gmail.com>
2024-04-25 22:24:43 +0200

    m

--- routing-demo/src/app/order-product/page.tsx
+++ routing-demo/src/app/order-product/page.tsx
@@ -7,7 +7,12 @@ export default function OrderProduct() {
 
     const handleClick = () => {
         console.log("Placing your order");
-        router.push("/"); // Note: navigate back to the home page
+        // router.push("/"); // Note: navigate back to the home page
+        // router.replace("/"); // Note: if we want to replace the navigation 
+        //                      //       history instead of pushing the route 
+        //                      //       onto the stack
+        // router.back();   // Note: navigate back to the router's history stack
+        router.forward();   // Note: navigate forward to the next page
     }
 
     // Note: <> means React fragment
